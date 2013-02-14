16f4f667efe36b7354d111bb9902feb44892b1ed
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 18:33:02 -0500

    dividing

--- bar.hs
+++ bar.hs
@@ -4,17 +4,25 @@ import qualified Data.List as L
 -- Bars to be plotted
 type Bars = M.Map String Int
 
-makeBar :: String -> Int -> String
-makeBar key value = label ++ " " ++ bar
+highestHeight :: Bars -> Int
+highestHeight bars = M.foldl (\a b -> maximum [a, b] ) 0 bars
+
+-- Count for the bars
+makeBars :: String -> Bars
+makeBars raw = M.map (\x -> round $ x / divisor) bars
+  where
+    bars = foldr (\level -> M.insertWith (+) level 1) M.empty $ words raw
+    divisor = 3
+
+-- Write one row.
+makeBarString :: String -> Int -> String
+makeBarString key value = label ++ " " ++ bar
   where
     bar = take value $ repeat '|'
     label = reverse $ take 5 $ reverse $ (take 5 $ repeat ' ') ++ key
 
 makeBarPlot :: Bars -> [String]
-makeBarPlot bars = map (\l -> snd l) $ M.toList $ M.mapWithKey makeBar bars
-
-makeBars :: String -> Bars
-makeBars raw = foldr (\level -> M.insertWith (+) level 1) M.empty $ words raw
+makeBarPlot bars = map (\l -> snd l) $ M.toList $ M.mapWithKey makeBarString bars
 
 barplot :: String -> String
 barplot raw = (L.intercalate "\n" $ makeBarPlot $ makeBars raw) ++ "\n"
--- readme.md
+++ readme.md
@@ -4,8 +4,4 @@ Plots for your shell
 ## Histograms
 Given a factor variable, plot a bar plot
 
-    $ cat ??? | ./bar
-
-Plot a histogram from stdin.
-
-    $ cat sepal | ./hist
+    $ cat postures | ./bar
