3014360cdf26b9641971dd6a901f73c5f1bd9757
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 17:00:41 -0500

    hist

--- Plot.hs
+++ Plot.hs
@@ -1,19 +1,21 @@
-import qualified Data.Map as Map
+import qualified Data.Map as M
+import qualified Data.List as L
 
 -- Bars to be plotted
-type Bars = Map.Map String Int
+type Bars = M.Map String Int
 
 -- An example
 sepalLengthBars :: Bars
-sepalLengthBars = Map.fromList [("4", 5), ("5", 47), ("6", 68), ("7", 24), ("8", 6)]
+sepalLengthBars = M.fromList [("4", 5), ("5", 47), ("6", 68), ("7", 24), ("8", 6)]
 
 makeBar :: String -> Int -> String
---makeBar key value = key ++ " " ++ take value $ repeat '='
-makeBar key value = take value $ repeat '='
+makeBar key value = key ++ " " ++ bar
+  where
+    bar = take value $ repeat '|'
 
---makeBarPlot :: Bars -> [String]
---makeBarPlot bars = Map.mapWithKey makeBar bars
+makeBarPlot :: Bars -> [String]
+makeBarPlot bars = map (\l -> snd l) $ M.toList $ M.mapWithKey makeBar bars
 
 main = do
---putStrLn $ show $ makeBarPlot sepalLengthBars
-  putStrLn $ makeBar "aoeu" 8
+  putStrLn $ L.intercalate "\n" $ makeBarPlot sepalLengthBars
+--putStrLn $ makeBar "aoeu" 8
