bf893ee1d3335e84d2ab0573f9fde99ec4518dd6
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 13:41:20 -0500

    typechecks

---
+++ Plot.hs
@@ -0,0 +1,22 @@
+import qualified Data.Map as Map
+
+-- Bars to be plotted
+type Bars = Map.Map String Int
+
+-- An example
+sepalLengthBars :: Bars
+sepalLengthBars = Map.fromList [("4", 5), ("5", 47), ("6", 68), ("7", 24), ("8", 6)]
+
+-- Convert bars to a list of lines
+makePlotLines :: Bars -> [String]
+makePlotLines bars = reverse [""]
+
+-- Make a plot
+countPlot :: Int -> Int -> Bars -> String
+countPlot height width bars = concat $ makePlotLines bars
+
+-- Default settings
+countPlotDefault = countPlot 50 80
+
+main = do
+  putStrLn $ countPlotDefault sepalLengthBars
