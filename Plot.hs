e6bd13ee8977bdb4e5bbcea66cc2db9979029b53
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 17:44:54 -0500

    yay

--- Plot.hs
+++ Plot.hs
@@ -16,17 +16,20 @@ makeBar key value = label ++ " " ++ bar
 makeBarPlot :: Bars -> [String]
 makeBarPlot bars = map (\l -> snd l) $ M.toList $ M.mapWithKey makeBar bars
 
+makeHistBars :: String -> Bars
+makeHistBars raw = foldr (\level -> M.insertWith (+) (show level) 0) bars [(minimum levels)..(maximum levels)]
+  where
+    bars = makeBars raw
+    levels = map (\x -> read x :: Int) $ M.keys bars
+
 barplot :: String -> String
 barplot raw = (L.intercalate "\n" $ makeBarPlot $ makeBars raw) ++ "\n"
 
---makeHistBars :: String -> Bars
---makeHistBars raw = map (\key -> M.insertWith (+) key 0) bars
---  where
---    bars = foldr (\level -> M.insertWith (+) level 1) M.empty $ map (\w -> read w :: Int) words raw
-
---hist :: String -> String
---hist raw = (L.intercalate "\n" $ makeBarPlot $ makeHistBars raw) ++ "\n"
+hist :: String -> String
+hist raw = (L.intercalate "\n" $ makeBarPlot $ makeHistBars raw) ++ "\n"
 
 --main = interact main'
 main = do
   putStrLn $ barplot "8 7 3 2 3 2 3 2 8"
+  putStrLn $ hist "8 7 3 2 3 2 3 2 8"
+  putStrLn $ show $ makeHistBars "8 7 3 2 3 2 3 2 8"
