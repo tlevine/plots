cf664be3551d40119fbe1a601da2bf9b6030d40a
Thomas Levine <occurrence@thomaslevine.com>
2013-02-14 17:16:52 -0500

    yay

--- Plot.hs
+++ Plot.hs
@@ -5,8 +5,16 @@ import qualified Data.List as L
 type Bars = M.Map String Int
 
 -- An example
-sepalLengthBars :: Bars
-sepalLengthBars = M.fromList [("4", 5), ("5", 47), ("6", 68), ("7", 24), ("8", 6)]
+sepalLengthBars' :: Bars
+sepalLengthBars' = M.fromList [("4", 5), ("5", 47), ("6", 68), ("7", 24), ("8", 6)]
+
+sepalLengthRaw :: String
+sepalLengthRaw = "4 4 4 4 5 5 5 5 6 7 8 7 9 6 6 5 4\n3    5 5 5 5 5 5 5"
+
+makeBars :: String -> Bars
+makeBars raw = foldr (\level -> M.insertWith (+) level 1) M.empty $ words raw
+
+sepalLengthBars = makeBars sepalLengthRaw
 
 makeBar :: String -> Int -> String
 makeBar key value = label ++ " " ++ bar
@@ -19,4 +27,3 @@ makeBarPlot bars = map (\l -> snd l) $ M.toList $ M.mapWithKey makeBar bars
 
 main = do
   putStrLn $ L.intercalate "\n" $ makeBarPlot sepalLengthBars
---putStrLn $ makeBar "aoeu" 8
