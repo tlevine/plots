085520a023600880fd051a1a538ace14ca6750a8
Thomas Levine <occurrence@thomaslevine.com>
2013-02-15 13:00:28 -0500

    counts

--- hist.hs
+++ hist.hs
@@ -4,39 +4,16 @@ import qualified Data.List as L
 -- Bars to be plotted
 type HistBars = M.Map Rational Int
 
---highestHeight :: Bars -> Int
---highestHeight bars = M.foldl (\a b -> maximum [a, b] ) 0 bars
---
----- Count for the bars
---makeBars :: String -> Bars
---makeBars raw = M.map (\x -> round $ x * 54 / divisor) bars
---  where
---    bars = foldr (\level -> M.insertWith (+) level 1) M.empty $ words raw
---    divisor = maximum $ map snd $ M.toList bars
---
----- Write one row.
---makeBarString :: Rational -> Int -> String
---makeBarString key value = label ++ " " ++ bar
---  where
---    bar = take value $ repeat '|'
---    label = show key
---
---makeBarPlot :: Bars -> [String]
---makeBarPlot bars = map (\l -> snd l) $ M.toList $ M.mapWithKey makeBarString bars
---
---barplot :: String -> String
---barplot raw = (L.intercalate "\n" $ makeBarPlot $ makeBars raw) ++ "\n"
+hist :: Rational -> Int -> [Rational] -> [(Rational, Int)]
+hist minBin nBins dataColumn = []
 
---main = interact barplot
-
--- Round to centers, and return a list of (center, count).
-binnedCount :: Rational -> [Rational] -> [(Rational, Int)]
-binnedCount interval x = map (\x' -> takeWhile 
-  xSorted = sort x
-
-breaks :: Rational -> Rational -> Rational -> [Rational]
-breaks nBreaks dataMin dataMax = take nBreaks $ L.unfoldr (\b -> Just (b, b+interval) firstBreak
+counts :: Rational -> Rational -> [Rational] -> [(Rational, Int)]
+counts minBin binWidth dataColumn = take nBins $ map (\b -> (b, binHeight b)) bins
   where
+    nBins = ceiling $ ((maximum dataColumn) - minBin) / binWidth
+    bins = take nBins $ L.unfoldr (\x -> Just (x, x + binWidth)) minBin
+    binHeight binLeft = length $ filter (\x -> x >= binLeft && x < (binLeft + binWidth)) dataColumn
+
 
 main = do
-  putStrLn $ show $ binnedCount [1..10] ++ [3..14] ++ [16,3,2,4]
+  putStrLn $ show $ counts 23 4 $ map (* 1.1) [26..50]
