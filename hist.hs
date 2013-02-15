b80484a8d87f70e2f23cd7b102528cfac224c5cb
Thomas Levine <occurrence@thomaslevine.com>
2013-02-15 12:46:57 -0500

    oeu

---
+++ hist.hs
@@ -0,0 +1,42 @@
+import qualified Data.Map as M
+import qualified Data.List as L
+
+-- Bars to be plotted
+type HistBars = M.Map Rational Int
+
+--highestHeight :: Bars -> Int
+--highestHeight bars = M.foldl (\a b -> maximum [a, b] ) 0 bars
+--
+---- Count for the bars
+--makeBars :: String -> Bars
+--makeBars raw = M.map (\x -> round $ x * 54 / divisor) bars
+--  where
+--    bars = foldr (\level -> M.insertWith (+) level 1) M.empty $ words raw
+--    divisor = maximum $ map snd $ M.toList bars
+--
+---- Write one row.
+--makeBarString :: Rational -> Int -> String
+--makeBarString key value = label ++ " " ++ bar
+--  where
+--    bar = take value $ repeat '|'
+--    label = show key
+--
+--makeBarPlot :: Bars -> [String]
+--makeBarPlot bars = map (\l -> snd l) $ M.toList $ M.mapWithKey makeBarString bars
+--
+--barplot :: String -> String
+--barplot raw = (L.intercalate "\n" $ makeBarPlot $ makeBars raw) ++ "\n"
+
+--main = interact barplot
+
+-- Round to centers, and return a list of (center, count).
+binnedCount :: Rational -> [Rational] -> [(Rational, Int)]
+binnedCount interval x = map (\x' -> takeWhile 
+  xSorted = sort x
+
+breaks :: Rational -> Rational -> Rational -> [Rational]
+breaks nBreaks dataMin dataMax = take nBreaks $ L.unfoldr (\b -> Just (b, b+interval) firstBreak
+  where
+
+main = do
+  putStrLn $ show $ binnedCount [1..10] ++ [3..14] ++ [16,3,2,4]
