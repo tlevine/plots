289f210efce11ce94822b0bdabc2591d69a19d7c
Thomas Levine <occurrence@thomaslevine.com>
2013-02-15 13:10:01 -0500

    aoeu

--- hist.hs
+++ hist.hs
@@ -2,18 +2,21 @@ import qualified Data.Map as M
 import qualified Data.List as L
 
 -- Bars to be plotted
-type HistBars = M.Map Rational Int
+type HistBars = M.Map Float Int
 
-hist :: Rational -> Int -> [Rational] -> [(Rational, Int)]
-hist minBin nBins dataColumn = []
+-- Draw one bar given the center and the count
 
-counts :: Rational -> Rational -> [Rational] -> [(Rational, Int)]
-counts minBin binWidth dataColumn = take nBins $ map (\b -> (b, binHeight b)) bins
+-- Compose the overall histogram.
+--hist :: Float -> Float -> [Float] -> [(Float, Int)]
+--hist minBin binWidth dataColumn = concat $ intersperse "\n" $ map writeBar counts minBin binWidth dataColumn
+
+-- Count and round.
+counts :: Float -> Float -> [Float] -> [(Float, Int)]
+counts minBin binWidth dataColumn = take nBins $ map (\b -> (b + (fromIntegral $ binHeight b) / 2, binHeight b)) bins
   where
     nBins = ceiling $ ((maximum dataColumn) - minBin) / binWidth
     bins = take nBins $ L.unfoldr (\x -> Just (x, x + binWidth)) minBin
     binHeight binLeft = length $ filter (\x -> x >= binLeft && x < (binLeft + binWidth)) dataColumn
-
 
 main = do
   putStrLn $ show $ counts 23 4 $ map (* 1.1) [26..50]
