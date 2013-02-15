bfb3bb6c0868e2970b2fe734a3cd5a96e512e5f8
Thomas Levine <occurrence@thomaslevine.com>
2013-02-15 13:26:10 -0500

    oeu

--- hist.hs
+++ hist.hs
@@ -1,12 +1,14 @@
 import qualified Data.List as L
+import qualified Text.Printf as P
 
 type Count = (Float, Int)
 
 -- Draw one bar given the center and the count
 writeBar :: Count -> String
-writeBar count = take c $ repeat '='
+writeBar count = (P.printf ("%." ++ resolution ++ "f") x) ++ ( take c $ repeat '=' )
   where
     (x,c) = count
+    resolution = maximum [0, 0 - (fromIntegral round $ log x)]
 
 -- Compose the overall histogram.
 hist :: Float -> Float -> [Float] -> String
@@ -22,4 +24,4 @@ counts minBin binWidth dataColumn = take nBins $ map (\b -> (b + (fromIntegral $
 
 main = do
   putStrLn $ show $ counts 23 4 $ map (* 1.1) [26..50]
-  putStrLn $ hist 23 4 $ map (* 1.1) [26..50]
+  putStrLn $ hist 23 4 $ map (* 1.1) [26..50] ++ [70..80]
